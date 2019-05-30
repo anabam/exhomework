197e7c19f74d091442b8c951797c6047e4c8e28d
Robert <rlarkin@sans.org>
2019-05-30 12:53:51 -0400

    routing

---
+++ routes/fibs.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const fibonacci = require ('fibonacci');
+
+router.get('/fibs:N', function(req, res, next) {
+  var N = req.params.N;
+  let response = await fibonacci(N);
+  res.send(response);
+});
+
+module.exports = router;
+
