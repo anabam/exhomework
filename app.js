84d7f7e15d6962c7a65298a1961a0f657d36a3a3
Robert <rlarkin@sans.org>
2019-05-30 13:08:45 -0400

    latest

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/fibs', fibsRouter);
+app.use('/fibs/:N', fibsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/fibs.js
+++ routes/fibs.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const fibonacci = require ('fibonacci');
 
-router.get('/fibs:N', function(req, res, next) {
+router.get('/fibs:N', async function(req, res, next) {
   var N = req.params.N;
   let response = await fibonacci(N);
   res.send(response);
