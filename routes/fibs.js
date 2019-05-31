8417bebb11a39d1b0c79203b1b913354edd6f651
Robert <rlarkin@sans.org>
2019-05-31 12:07:56 -0400

    update fibs for sequence

--- routes/fibs.js
+++ routes/fibs.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const fibonacci = require ('fibonacci');
+const fibtastic = require("fibtastic");
 
 router.get('/:N', async function(req, res, next) {
+  const startNumber = 1;
   var N = req.params.N;
-  let response = await fibonacci.iterate(N);
+  let response = {};
+  response.sequence = await fibtastic.sequence(startNumber, N);
+  response.report = await fibonacci.iterate(N);
+  //let response = await fibonacci.iterate(N);
   res.send(response);
 });
 
